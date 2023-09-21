a239113340f9fb2ef0f12a774870626f041538ef
RageNeko26 <rneko2006@gmail.com>
2023-09-22 00:39:48 +0700

    refactor: Change method and import file names

--- src/pages/api/v1.ts
+++ src/pages/api/v1.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { InitializeDB } from "../../model/book"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  InitializeDB()
+
   res.json({
     message: "Nextjs API Version 1.0.0",
     status: "success"
